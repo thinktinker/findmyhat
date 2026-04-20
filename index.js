b5326f486560783777c01aaac31a4b402bb486d3
thinktinker <martinleongmo@gmail.com>
2026-04-20 14:32:38 +0800

    create game file structure and dependencies

---
+++ index.js
@@ -0,0 +1,33 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+// TODO: Game elements/assets constants
+// TODO: UP / DOWN / LEFT / RIGHT / QUIT keyboard constants
+// TODO: MSG_UP / MSG_DOWN / MSG_LEFT / MSG_RIGHT / MSG_ QUIT / MSG_INVALID message constants
+// TODO: WIN / LOSE / OUT / QUIT messages constants
+// TODO: MAP ROWS, COLUMNS AND PERCENTAGE
+
+class Field {
+  // TODO: constructor, a built-in method of a class (invoked when an object of a class is instantiated)
+
+  // TODO: generateField is a static method, returning a 2D array of the fields
+
+  // TODO: welcomeMessage is a static method, displays a string
+
+  // TODO: setHat positions the hat along a random x and y position within field array
+
+  // TODO: printField displays the updated status of the field position
+
+  // TODO: updateMove displays the move (key) entered by the user
+
+  // !! TODO: updateGame Assessment Challenge
+
+  //  TODO: start() a public method of the class to start the game
+}
+
+// TODO: Generate a new field - using Field's static method: generateField
+
+// TODO: Generate aa welcome message
+
+// TODO: Create a new instance of the game
+
+// TODO: Invoke method start(...) from the instance of game object
